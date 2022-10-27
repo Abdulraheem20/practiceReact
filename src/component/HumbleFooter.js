29bb6f5775cac1bd4a690e27268a4ceb86150cbc
Abdulraheem <raimitaiwo45@gmail.com>
2022-10-27 17:18:51 +0100

    newCommit

--- src/component/HumbleFooter.js
+++ src/component/HumbleFooter.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import './styles/Footer.css'
-import {FaFacebook} from 'react-icons/fa'
-import {AiFillInstagram} from 'react-icons/ai'
-import {AiFillTwitterCircle} from 'react-icons/ai'
+import React from "react";
+import "./styles/Footer.css";
+import { FaFacebook } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { AiFillTwitterCircle } from "react-icons/ai";
 const FooterMQ = () => {
   return (
-    <div className='footer'>
-        <h1>LOGO</h1>
-        <div className='social-media'>
-            <div>
-            <FaFacebook className='fb'/>
-            </div>
-            <div>
-            <AiFillInstagram className='insta'/>
-            </div>
-            <div>
-            <AiFillTwitterCircle className='tweet'/>
-            </div>
+    <div className="footer">
+      <h1>LOGO</h1>
+      <div className="social-media">
+        <div>
+          <FaFacebook className="fb" />
         </div>
+        <div>
+          <AiFillInstagram className="insta" />
+        </div>
+        <div>
+          <AiFillTwitterCircle className="tweet" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterMQ
+export default FooterMQ;
